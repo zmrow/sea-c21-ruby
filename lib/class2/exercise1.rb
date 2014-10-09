d3f5b573e45445c195d2c890607c0e5b66583e88
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-08 20:50:06 -0700

    Fix exercise 1 and bundler/rubocop jazz

--- lib/class2/exercise1.rb
+++ lib/class2/exercise1.rb
@@ -4,8 +4,5 @@
 #
 # TIP: I expect you to use a mathematical equation.
 #
-hours_in_a_day = 24
-days_in_a_year = 365
-hours_in_a_year = hours_in_a_day * days_in_a_year
 
-puts 'There are ' + hours_in_a_year.to_s + ' hours in a year!'
+puts 24 * 365
