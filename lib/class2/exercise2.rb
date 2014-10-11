e31b19d5336988d34a770a756369d7c3fd6295ba
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-11 11:04:18 -0700

    Fix exercise 2 and 3

--- lib/class2/exercise2.rb
+++ lib/class2/exercise2.rb
@@ -5,13 +5,5 @@
 # TIP: I expect you to use a mathematical equation.
 #
 
-min = 60
-hours = 24
-days_in_year = 365
-decade = 10
-min_in_day = min * hours
-min_in_year = min_in_day * days_in_year
-min_in_decade = min_in_year * decade
-
-puts 'There are ' + min_in_decade.to_s + ' minutes in a decade.'
+puts (((60 * 24) * 365) * 10)
 
