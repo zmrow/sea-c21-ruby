393801ac70a2a91439c61137fbfe9960f8c2cc53
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-13 21:04:15 -0700

    Finish first class 3 exercise

--- lib/class3/exercise1.rb
+++ lib/class3/exercise1.rb
@@ -16,3 +16,27 @@
 #   Go to the store and buy some more, 3 bottles of beer on the wall!
 #
 # TIP: I expect you to use a loop.
+
+bottles = 3
+word = 'bottles'
+
+loop do
+  puts "#{bottles} #{word} of beer on the wall, #{bottles} #{word} of beer!"
+
+  bottles -= 1
+
+  bottles = 'no more' if bottles == 0
+
+  if bottles == 1
+    word = 'bottle'
+  else
+    word = 'bottles'
+  end
+
+  puts "Take one down, pass it around, #{bottles} #{word} of beer on the wall!"
+
+  break if bottles == 'no more'
+end
+
+puts 'No more bottles of beer on the wall, no more bottles of beer!'
+puts 'Go to the store and buy some more, 3 bottles of beer on the wall!'
--- scratch.rb
+++ scratch.rb
@@ -1,2 +1,23 @@
 # The secret of getting ahead is getting started.
 # - Mark Twain
+#
+while true
+  puts 'What would you like to ask C to do?'
+  request = gets.chomp
+
+  puts 'You say, "C, please ' + request + '"'
+
+  puts "C's response:"
+  puts '"Papa ' + request + '."'
+  puts '"Mama ' + request + '."'
+  puts '"Ruby ' + request + '."'
+  puts '"Nono ' + request + '."'
+  puts '"Emma ' + request + '."'
+  puts
+
+  if request == 'stop'
+    break
+  end
+end
+
+
