cba56dab165d1a26849c47087f182d3622bdfb67
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-19 19:22:01 -0700

    Finish class 4 exercise 2

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -17,3 +17,12 @@
 # TIP #2: Use the `string.ljust` method from class 2 exercise 7.
 #
 # TIP #3: The width of each line is 50 characters.
+song = ['Can I Kick It?', 'Rump Shaker', 'Check Yo Self', \
+        'Regulate', 'I Got 5 On It', 'Ready Or Not']
+group = ['A Tribe Called Quest', 'Wreckx-n-Effect', 'Ice Cube', \
+         'Warren G & Nate Dogg', 'Luniz', 'The Fugees']
+y = [1991, 1992, 1993, 1994, 1995, 1996]
+
+(0..5).each do |x|
+  puts "#{song[x]}".ljust(20) + "#{group[x]}".ljust(26) + "#{y[x]}".ljust(4)
+end
