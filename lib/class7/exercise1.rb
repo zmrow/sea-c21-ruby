f87166b4a3f3a25eff3ac8acf720f7eaf8aab12b
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-27 21:20:38 -0700

    Finish class 7 exercise 1

--- lib/class7/exercise1.rb
+++ lib/class7/exercise1.rb
@@ -47,28 +47,43 @@
 
 class Integer
   def hours_in_seconds
-    # replace me
+    self * 60 * 60
   end
 end
 
 class String
   def indent(amount = 2)
-    amount # replace me
+    ' ' * amount + self
   end
 end
 
 class Integer
   def to_roman
-    # replace me
+    roms = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', \
+             100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', \
+             9 => 'IX', 8 => 'VIII', 7 => 'VII', 6 => 'VI', 5 => 'V', \
+             4 => 'IV', 1 => 'I' }
+
+    num = self
+    roman_num = ''
+
+    while num > 0
+      roms.each do |key, value|
+        roman_num += value * (num / key)
+        num = num % key
+      end
+    end
+    roman_num
+
   end
 end
 
 class Array
   def second
-    # replace me
+    self[1]
   end
 
   def third
-    # replace me
+    self[2]
   end
 end
